f364f6fb6bb35a06d7938aa0d9cfba66cb8195a7
skymagenta <jokero.sky1@gmail.com>
2022-05-21 18:44:12 +0300

    fix games/calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -3,20 +3,17 @@
 from random import randint, choice
 
 
-def calc(game_count=3):
-    questions = []
-    correct_answers = []
-    for i in range(game_count):
-        x = randint(1, 100)
-        y = randint(1, 100)
-        sign = choice(['+', '-', '*'])
-        questions.append(f"{x} {sign} {y}")
-
-        if sign == '+':
-            correct_answers.append(str(x + y))
-        elif sign == '-':
-            correct_answers.append(str(x - y))
-        else:
-            correct_answers.append(str(x * y))
-    print('What is the result of the expression?')
-    return questions, correct_answers
+def calc():
+    game_conditions = 'What is the result of the expression?'
+    questions_and_answers = []
+    x = randint(1, 100)
+    y = randint(1, 100)
+    sign = choice(['+', '-', '*'])
+    questions_and_answers.append(f"{x} {sign} {y}")
+    if sign == '+':
+        questions_and_answers.append(str(x + y))
+    elif sign == '-':
+        questions_and_answers.append(str(x - y))
+    else:
+        questions_and_answers.append(str(x * y))
+    return questions_and_answers, game_conditions
