99e5021baaa625945c35db3752bf1503beb78d84
skymagenta <jokero.sky1@gmail.com>
2022-05-21 18:53:54 +0300

    fix scripts/brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-from brain_games.realization import machinarium
+from brain_games.game_engine import launch_game
 from brain_games.games.prime import prime
 
 
 def main():
-    machinarium(prime)
+    launch_game(prime)
 
 
 if __name__ == '__main__':
