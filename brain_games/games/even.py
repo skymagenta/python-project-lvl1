9650c1e4d1eb90ff6b73de15ffdd185f35a697f3
skymagenta <jokero.sky1@gmail.com>
2022-05-21 18:45:28 +0300

    fix games/even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -3,16 +3,13 @@
 from random import randint
 
 
-def even(game_count=3):
-    questions = []
-    correct_answers = []
-    for i in range(game_count):
-        x = randint(1, 100)
-        questions.append(str(x))
-
-        if x % 2 == 0:
-            correct_answers.append('yes')
-        else:
-            correct_answers.append('no')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    return questions, correct_answers
+def even():
+    game_conditions = 'Answer "yes" if the number is even, otherwise answer "no".'
+    questions_and_answers = []
+    x = randint(1, 100)
+    questions_and_answers.append(x)
+    if x % 2 == 0:
+        questions_and_answers.append('yes')
+    else:
+        questions_and_answers.append('no')
+    return questions_and_answers, game_conditions
