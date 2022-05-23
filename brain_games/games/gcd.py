b7eeeedc689514acbdd58af587a3af55b7281a5b
skymagenta <jokero.sky1@gmail.com>
2022-05-23 22:40:41 +0300

    fix games/gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python
 
 from random import randint
+from math import gcd
+
+GAME_CONDITIONS = 'Find the greatest common divisor of given numbers.'
 
 
-def get_greatest_common_divisor(x, y):
-    """
-    Finds the greatest common divisor of two positive integers.
-    """
-    for num in range(min(x, y), 0, -1):
-        if (x % num == 0) and (y % num == 0):
-            gcd = num
-            break
-    return gcd
-
-
-def gcd():
-    game_conditions = 'Find the greatest common divisor of given numbers.'
-    questions_and_answers = []
+def game():
     x = randint(1, 100)
     y = randint(1, 100)
-    questions_and_answers.append(f"{x} {y}")
-    questions_and_answers.append(str(get_greatest_common_divisor(x, y)))
-    return questions_and_answers, game_conditions
+    question = f"{x} {y}"
+    answer = str(gcd(x, y))
+    return question, answer
