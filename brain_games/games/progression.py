dfc5f6d77e2fa7da15a05d11a350506e3bb6c09d
skymagenta <jokero.sky1@gmail.com>
2022-05-23 22:42:46 +0300

    fix games/progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,32 +2,29 @@
 
 from random import randint
 
+GAME_CONDITIONS = 'What number is missing in the progression?'
 
-def create_sequence():
+
+def create_progression():
     """
     Creates a list of 10 numbers ranging from 1 to 99
     in an arithmetic progression.
     """
     first_element = randint(1, 29)
     step = randint(2, 7)
-    list_of_num = []
-    max_elements = 10
-    added_element = first_element
-    for i in range(max_elements):
-        list_of_num.append(str(added_element))
-        added_element += step
+    progression = []
+    progression_size = 10
+    for i in range(progression_size):
+        progression.append(str(first_element))
+        first_element += step
 
-    return list_of_num
+    return progression
 
 
-def progression():
-    game_conditions = 'What number is missing in the progression?'
-    questions_and_answers = []
-    list_of_num = create_sequence()
-    position = randint(0, len(list_of_num) - 1)
-    correct_answer = list_of_num[position]
-    list_of_num[position] = '..'
-    question = ' '.join(list_of_num)
-    questions_and_answers.append(question)
-    questions_and_answers.append(correct_answer)
-    return questions_and_answers, game_conditions
+def game():
+    progression = create_progression()
+    random_index = randint(0, len(progression) - 1)
+    answer = progression[random_index]
+    progression[random_index] = '..'
+    question = ' '.join(progression)
+    return question, answer
