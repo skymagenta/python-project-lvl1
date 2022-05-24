62a03e910e812fe9e3d77b156d89a36d86f7e83c
skymagenta <jokero.sky1@gmail.com>
2022-05-24 21:47:14 +0300

    fix games/calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,19 +1,17 @@
-#!/usr/bin/env python
-
 from random import randint, choice
 
-GAME_CONDITIONS = 'What is the result of the expression?'
+RULE = 'What is the result of the expression?'
 
 
-def game():
+def game_round():
     x = randint(1, 100)
     y = randint(1, 100)
     random_operator = choice(['+', '-', '*'])
     question = f"{x} {random_operator} {y}"
     if random_operator == '+':
-        answer = str(x + y)
+        answer = x + y
     elif random_operator == '-':
-        answer = str(x - y)
+        answer = x - y
     elif random_operator == '*':
-        answer = str(x * y)
-    return question, answer
+        answer = x * y
+    return question, str(answer)
