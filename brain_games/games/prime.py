ee069bee585863903ddaa1d40e07f1a1d6875ff4
skymagenta <jokero.sky1@gmail.com>
2022-05-25 21:26:20 +0300

    fix games/prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,21 +1,21 @@
 from random import randint
+from math import sqrt, ceil
 
 RULE = 'Answer "yes" if given number is prime. \
 Otherwise answer "no".'
 
 
-def is_number_prime(num):
+def is_prime(num):
     """
     Checks, is number prime.
     """
     if num <= 1:
         return False
-    elif num == 2:
-        return True
-    else:
-        for divider in range(2, num):
-            if num % divider == 0:
-                return False
+    # elif num == 2:
+        # return True
+    for divider in range(2, ceil(sqrt(num) + 1)):
+        if num % divider == 0:
+            return False
 
     return True
 
@@ -23,9 +23,11 @@ def is_number_prime(num):
 def game_round():
     num = randint(1, 100)
     question = num
-    answer = is_number_prime(num)
-    if answer is True:
+    answer = 'no'
+    # answer = is_number_prime(num)
+    # if is_prime(num) is True:
+    if is_prime(num):
         answer = 'yes'
-    else:
-        answer = 'no'
+    # else:
+        # answer = 'no'
     return question, answer
