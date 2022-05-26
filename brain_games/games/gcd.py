36b7785f62443f7c892047e2ddfa0a8ec61c2944
skymagenta <jokero.sky1@gmail.com>
2022-05-26 23:07:25 +0300

    fix games/gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -5,8 +5,8 @@ RULE = 'Find the greatest common divisor of given numbers.'
 
 
 def game_round():
-    x = randint(1, 100)
-    y = randint(1, 100)
-    question = f"{x} {y}"
-    answer = str(gcd(x, y))
+    random_number_1 = randint(1, 100)
+    random_number_2 = randint(1, 100)
+    question = f"{random_number_1} {random_number_2}"
+    answer = str(gcd(random_number_1, random_number_2))
     return question, answer
